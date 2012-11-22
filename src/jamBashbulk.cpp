64c2c26a901f93408fea3ca01d192ac1e3dcbf02
Merlijn van Deen <valhallasw+lisilwen@gmail.com>
2012-11-22 15:48:22 +0100

    Rename 'extraPositionArray' to more sensible 'pAfterChange'

--- src/jamBashbulk.cpp
+++ src/jamBashbulk.cpp
@@ -480,8 +480,8 @@ void calcShearModulus()
     bool sufficientAccuracy = false;
     int numberOfDataPoints = 0;
 
-    vector<long double> extraPositionArray;
-    extraPositionArray.reserve(2 * N + 3);
+    vector<long double> pAfterChange;
+    pAfterChange.reserve(2 * N + 3);
 
     long double shearLast = 0.0, sxyLast = sxy;
 
@@ -619,7 +619,7 @@ void calcShearModulus()
                     num++;
 
                     jloop(2 * N + 3) {
-                        extraPositionArray[j] = p[j];
+                        pAfterChange[j] = p[j];
                     } // save positions just after rearrangement
                     jloop(2 * N + 3) {
                         p[j] = pLast[j];
@@ -726,9 +726,9 @@ void calcShearModulus()
             }
         } // end while(!sufficientAccuracy)
 
-        // extraPositionArray is saved... some 100 lines above here
+        // pAfterChange is saved... some 100 lines above here
         jloop(2 * N + 3) {
-            p[j] = extraPositionArray[j];
+            p[j] = pAfterChange[j];
         }
 
         if(!fixedStepSize) {
@@ -777,8 +777,8 @@ void calcBulkModulus()
     bool sufficientAccuracy = false;
     int numberOfDataPoints = 0;
 
-    vector<long double> extraPositionArray;
-    extraPositionArray.reserve(2 * N + 3);
+    vector<long double> pAfterChange;
+    pAfterChange.reserve(2 * N + 3);
 
     long double shearLast = 0.0, sxyLast = sxy;
 
@@ -962,7 +962,7 @@ void calcBulkModulus()
                     num++;
 
                     jloop(2 * N + 3) {
-                        extraPositionArray[j] = p[j];
+                        pAfterChange[j] = p[j];
                     } // save positions just after rearrangement
                     jloop(2 * N + 3) {
                         p[j] = pLast[j];
@@ -1114,7 +1114,7 @@ void calcBulkModulus()
         writeMultiplePackings(GpositionFile);
 
         jloop(2 * N + 3) {
-            p[j] = extraPositionArray[j];
+            p[j] = pAfterChange[j];
         }
         writeMultiplePackings(GpositionFile);
 
