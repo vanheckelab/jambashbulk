c7b18eada3223addba36b079940fc28a019e6090
Merlijn van Deen <valhallasw+novamaris@gmail.com>
2014-10-23 15:45:46 +0200

    Managementscripts hipper gemaakt

--- check_uptimes.py
+++ check_uptimes.py
@@ -1,4 +1,6 @@
-import subprocess, sys
+import subprocess, sys, itertools, getpass
+
+username = getpass.getuser()
 
 usage_error = False
 allservers = ["maris%03i" % i for i in range(2, 68)]
@@ -16,10 +18,26 @@ if len(sys.argv) == 1 or usage_error:
     print ""
     servers = allservers
 
+if "-v" in sys.argv:
+  verbose = True
+else:
+  verbose = False
 
-processes = [subprocess.Popen(["ssh", server, "cat /proc/cpuinfo | grep processor | wc -l && uptime"], stdout=subprocess.PIPE, stderr=open("/dev/null", "w")) for server in servers]
+justown = False
+if "--own" in sys.argv:
+  justown = True
+  verbose = True
+
+thekilling = False
+if "--kill" in sys.argv:
+  thekilling = True
+  verbose = True
+  justown = True
+
+processes = [subprocess.Popen(["ssh", server, "cat /proc/cpuinfo | grep processor | wc -l && uptime && ps -eo pcpu,pid,user,args | awk '{if ($1 > 95) print $0}'"], stdout=subprocess.PIPE, stderr=open("/dev/null", "w")) for server in servers]
 
 total_cpu_available = 0
+total_cpu = 0
 total_serv_available = 0
 for server,process in zip(servers, processes):
     data = process.stdout.read().strip().split("\n")
@@ -29,12 +47,37 @@ for server,process in zip(servers, processes):
     total_serv_available += 1
     uptime = data[1].strip()
     nprocs = int(data[0].strip())
-    load = round(float(data[1].split()[-1]))
+    load = round(float(data[1].split()[-3][:-1]))
     available = int(nprocs - load)
     if available < 0:
         available = 0
+    total_cpu += nprocs
     total_cpu_available += available
     total_serv_available += 1
-    print server, "[% 2i/% 2i processors available]" % (available, nprocs), uptime
+    print server, "[% 2i/% 2i processors available]\t" % (available, nprocs),
 
-print total_cpu_available, "processors available on", total_serv_available, "hosts"
+    processes = [d.split(None, 3) for d in data[2:]]
+    if not verbose:
+       # summarize processes
+       key = lambda x: x[2]
+       users = []
+       for user, procs in itertools.groupby(sorted(processes, key=key), key=key):
+           totalcpu = sum(float(p[0]) for p in procs)
+           users.append((user, totalcpu))
+       print "\t".join("%s (%.1f%%)" % (user, totalcpu) for (user, totalcpu) in users)
+    else:
+       print "" # newline
+       tokill = []
+       for process in sorted(processes):
+           if process[2] == username or not justown:
+               print "\t".join(process)
+               tokill.append(process)
+       if thekilling and tokill:
+           kill = raw_input("Kill %i processes? [y/n]" % len(tokill)).upper()
+           if kill == "Y":
+               subprocess.Popen(["ssh", server, "kill " + " ".join(str(pid) for pcpu,pid,user,args in tokill)], stderr=open("/dev/null", "w"))
+       
+           
+
+print total_cpu_available, "/", total_cpu, "processors available on", total_serv_available, "hosts (%i%%)" % (((total_cpu_available) * 100) / total_cpu)
+print total_cpu - total_cpu_available, "processors in use (%i%%)" % (((total_cpu - total_cpu_available) * 100) / total_cpu)
--- cleardats.py
+++ cleardats.py
@@ -5,8 +5,22 @@ errfiles = glob.glob("*.err")
 all = set(x.split(".")[0] for x in datfiles)
 running = set(x.split(".")[0] for x in errfiles)
 
+print "checking .err files for irrelevant errors...",
+i = 0
+for f in running:
+   err = open(f + ".err").readlines()
+   if "ParserWarning: Falling back to the 'python' engine because the 'c' engine does not support regex separators; you can avoid this warning by specifying engine='python'" in err[0] and len(err) == 2:
+      os.unlink(f+".err")
+      i += 1
+
+print i, "found."
+errfiles = glob.glob("*.err")
+running = set(x.split(".")[0] for x in errfiles)
+
 removable = all - running
 print "%i .dat files, of which we can remove %i (%i still running)" % (len(all), len(removable), len(running))
 
 for f in removable:
    os.unlink(f + ".dat")
+
+
